a86146892873b86f2743de4be82b680b3925609f
Hadrian Hughes <hadrian.hughes@and.digital>
2020-09-18 14:26:26 +0100

    Add unit test for children prop in Layout component

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -5,7 +5,14 @@ import Layout from './Layout'
 describe('Layout component', () => {
   it('Should render a <header> and <main>', () => {
     const wrapper = shallow(<Layout />)
+
     expect(wrapper.find('header').exists).toBeTruthy()
     expect(wrapper.find('main').exists).toBeTruthy()
+  })
+
+  it('Should render its children inside the <main> tag', () => {
+    const wrapper = shallow(<Layout><div id="render-me" /></Layout>)
+
+    expect(wrapper.find('main #render-me').exists).toBeTruthy()
   })
 })
