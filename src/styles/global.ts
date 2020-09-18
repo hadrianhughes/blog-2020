17c2dae220e3575c8b0ac2953ae74cf14ebcd445
Hadrian Hughes <hadrian.hughes@and.digital>
2020-09-18 15:09:26 +0100

    Set up Merriweather fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,8 +2,33 @@ import { createGlobalStyle } from 'styled-components'
 
 const Globals = createGlobalStyle`
   html, body {
+    font-family: Merriweather;
     font-size: 16px;
     margin: 0;
+  }
+
+  @font-face {
+    font-family: Merriweather;
+    src: url('/fonts/Merriweather-Regular.ttf');
+  }
+
+  @font-face {
+    font-family: Merriweather;
+    font-weight: bold;
+    src: url('/fonts/Merriweather-Bold.ttf');
+  }
+
+  @font-face {
+    font-family: Merriweather;
+    font-style: italic;
+    src: url('/fonts/Merriweather-Italic.ttf');
+  }
+
+  @font-face {
+    font-family: Merriweather;
+    font-style: italic;
+    font-weight: bold;
+    src: url('/fonts/Merriweather-BoldItalic.ttf');
   }
 `
 
