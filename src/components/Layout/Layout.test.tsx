ba0f0dab335f16e8386b5ff918a29219214d3c4a
Hadrian Hughes <hadrian.hughes@and.digital>
2020-09-19 13:34:17 +0100

    Only render Profile on breakpoints above mobile

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -4,6 +4,11 @@ import Layout from './Layout'
 import Profile from '../Profile'
 import { Header, Main } from './styles'
 
+jest.mock('react-responsive', () => ({
+  useMediaQuery: jest.fn()
+}))
+import { useMediaQuery } from 'react-responsive'
+
 describe('Layout component', () => {
   it('Should render a <Header> and <Main>', () => {
     const wrapper = shallow(<Layout />)
@@ -18,9 +23,17 @@ describe('Layout component', () => {
     expect(wrapper.find(Main).find('#render-me').exists()).toBeTruthy()
   })
 
-  it('Should render a <Profile> inside the <Header>', () => {
+  it('Should render a <Profile> inside the <Header> at desktop', () => {
+    useMediaQuery.mockImplementation(() => true)
     const wrapper = shallow(<Layout />)
 
     expect(wrapper.find(Profile).exists()).toBeTruthy()
+  })
+
+  it('Should not render <Profile> by default at mobile', () => {
+    useMediaQuery.mockImplementation(() => false)
+    const wrapper = shallow(<Layout />)
+
+    expect(wrapper.find(Profile).exists()).not.toBeTruthy()
   })
 })
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,36 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
 import GlobalStyles from '../../styles/global'
+import { breakpoints } from '../../styles/settings'
 import Profile from '../Profile'
 import { Container, Header, Main } from './styles'
 
-const Layout: FunctionComponent = ({ children }) => (
-  <Container>
-    <GlobalStyles />
-    <Header>
-      <Profile />
-      <hr />
-    </Header>
-    <Main>
-      {children}
-    </Main>
-  </Container>
-)
+const Layout: FunctionComponent = ({ children }) => {
+  const [isOpen, setOpen] = useState(false)
+  const isBigScreen = useMediaQuery({
+    query: `(min-width: ${breakpoints.small + 1}px)`
+  })
+
+  return (
+    <Container>
+      <GlobalStyles />
+      <Header>
+        {
+          isBigScreen || isOpen
+            ? (
+              <>
+                <Profile />
+                <hr />
+              </>
+            )
+            : null
+        }
+      </Header>
+      <Main>
+        {children}
+      </Main>
+    </Container>
+  )
+}
 
 export default Layout
