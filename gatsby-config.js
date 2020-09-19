c9aebbdc08ff60a316248abce8b37ad06f95ce09
Hadrian Hughes <hadrian.hughes@and.digital>
2020-09-19 11:20:44 +0100

    Install gatsby-source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,19 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  host: 'cdn.contentful.com',
+  environment: 'master'
+}
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig
+    }
+  ],
 }
