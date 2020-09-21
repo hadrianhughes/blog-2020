76eef7e17c8131b7bde6d1513203d2185a2c9b8c
Hadrian Hughes <hadrian.hughes@and.digital>
2020-09-21 14:55:52 +0100

    Add query to Homepage to fetch all article titles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 
-const Home = () => (
+interface HomeProps {
+  data: {
+    allContentfulArticle: {
+      edges: { title: string }[]
+    }
+  }
+}
+
+const Home: FunctionComponent<HomeProps> = ({ data }) => (
   <Layout>
     <div />
   </Layout>
 )
+
+export const query = graphql`
+  query HomeQuery {
+    allContentfulArticle(sort: { fields: createdAt }) {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+`
 
 export default Home
