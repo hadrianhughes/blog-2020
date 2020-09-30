d4babf3cba87cd0bdc54cc1f744d79891eb395b9
Hadrian Hughes <hadrian.hughes@and.digital>
2020-09-30 16:15:47 +0100

    Improve spacing in TagSelector

--- src/components/TagSelector/styles.ts
+++ src/components/TagSelector/styles.ts
@@ -5,22 +5,16 @@ import { Link } from 'gatsby'
 
 export const List = styled.ul`
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
   list-style-type: none;
   padding: 0;
 `
 
 export const Item = styled.li`
   font-family: 'Open Sans', sans-serif;
-  margin: 0 ${rem(10)};
+  margin: ${rem(5)};
   overflow: hidden;
-
-  &:first-child {
-    margin-left: 0;
-  }
-
-  &:last-child {
-    margin-right: 0;
-  }
 `
 
 export const StyledLink = styled(Link)`
@@ -43,12 +37,11 @@ export const StyledLink = styled(Link)`
     `
   }
 
-  border-radius: 20px;
   box-sizing: border-box;
   display: block;
   font-weight: bold;
   height: 100%;
-  padding: ${rem(10, 20)};
+  padding: ${rem(5, 10)};
   text-decoration: none;
   transition: background-color 0.5s, border 0.5s;
   width: 100%;
