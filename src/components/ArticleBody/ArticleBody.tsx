1b52a311b3d037f34b66ed6cc7e074e58348a481
Hadrian Hughes <hadrian.hughes@and.digital>
2020-12-11 16:24:16 +0000

    Re-incorporate custom renderers

--- src/components/ArticleBody/ArticleBody.tsx
+++ src/components/ArticleBody/ArticleBody.tsx
@@ -10,18 +10,17 @@ interface ArticleBodyProps {
   markdown: string
 }
 
-const options = {
-  renderNode: {
-    [BLOCKS.PARAGRAPH]: (_, children) => <Text tag="p" align="justify">{children}</Text>,
-    [BLOCKS.UL_LIST]: (_, children) => <UL>{children}</UL>,
-    [BLOCKS.LIST_ITEM]: (_, children) => <BulletItem>{children}</BulletItem>
-  }
+const renderers = {
+  code: CodeBlock,
+  paragraph: ({ children }) => <Text tag="p" align="justify">{children}</Text>,
+  list: ({ children }) => <UL>{children}</UL>,
+  listItem: ({ children }) => <BulletItem>{children}</BulletItem>
 }
 
 const ArticleBody: FunctionComponent<ArticleBodyProps> = ({ heading, markdown }) => (
   <Container>
     <Text tag="h1" size="headline">{heading}</Text>
-    <Markdown renderers={{ code: CodeBlock }}>
+    <Markdown renderers={renderers}>
       {markdown}
     </Markdown>
   </Container>
