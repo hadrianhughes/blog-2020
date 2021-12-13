1e04754d92cf59080cbf0e5f89856b4323eece99
Hadrian Hughes <hadrianhughes@gmail.com>
2021-12-13 17:14:16 +0000

    Enable fetching content from Contentful preview API

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
-  host: 'cdn.contentful.com'
+  host: `${process.env.IS_PREVIEW ? 'preview' : 'cdn'}.contentful.com`
 }
 
 module.exports = {
